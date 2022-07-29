d330b991259446f9cc8e9c8caa47946fabbeffba
Ankit Kumar Singh <ankit_singh@outlook.in>
2022-07-30 00:04:05 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (38.76%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,11 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int j=0;
-        for (int i=m-1;i<nums1.length-1;i++){
-            nums1[i+1]=nums2[j];
-            j++;
+        int currentP=m+n-1;
+        int num1P=m-1;
+        int num2P=n-1;
+        while(num1P>=0 && num2P>=0){
+            if(nums1[num1P]>nums2[num2P]){
+                nums1[currentP]=nums1[num1P];
+                num1P--;
+                currentP--;
+            }
+            else{
+                 nums1[currentP]=nums2[num2P];
+                num2P--;
+                currentP--;
+            }  
         }
-        Arrays.sort(nums1); 
-          System.out.println(nums1);
+    while(num2P>=0){
+    nums1[currentP]=nums2[num2P];
+        currentP--;
+        num2P--;
+    } 
     }
+    
 }
