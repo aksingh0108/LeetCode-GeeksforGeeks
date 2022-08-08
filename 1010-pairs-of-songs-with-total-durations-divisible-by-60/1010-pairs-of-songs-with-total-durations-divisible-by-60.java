7063e81ecd6da92ac67db71ed7363351bfdc983e
Ankit Kumar Singh <ankit_singh@outlook.in>
2022-08-08 22:26:17 +0530

    Time: 3 ms (95.33%), Space: 54.2 MB (58.35%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,11 +1,12 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
-        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        // Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        int array[]=new int[60];
         int count=0; 
         for(int i=0;i<time.length;i++){ 
-            if(time[i]%60==0)count+=map.getOrDefault(0,0);
-            else count+=map.getOrDefault(60-time[i] % 60,0);
-            map.put(time[i]%60,map.getOrDefault(time[i]%60,0)+1);
+            if(time[i]%60==0)count+=array[0];
+            else count+=array[60-time[i]%60];
+            array[time[i]%60]++;
             }
          return count;
     }
