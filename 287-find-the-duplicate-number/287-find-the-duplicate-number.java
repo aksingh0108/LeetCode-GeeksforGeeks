b30b8fb09c874b6c9048c256e2fb303a4033dc20
Ankit Kumar Singh <ankit_singh@outlook.in>
2022-08-16 23:44:11 +0530

    Time: 8 ms (55.94%), Space: 76.5 MB (27.08%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -28,12 +28,24 @@ class Solution {
         //     else set.add(nums[i]); 
         // }
         
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1])return nums[i];
-        }
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++){
+        //     if(nums[i]==nums[i+1])return nums[i];
+        // }
         
+        int slow=nums[0];
+        int fast=nums[0];
+        
+         do {
+              slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
          
-        return 0;
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
     }
 }
