1f4cdbbeafd14fe82c68a235d85c6fb2a9e2f0b8
Ankit Kumar Singh <ankit_singh@outlook.in>
2022-10-10 18:42:14 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (67.94%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,23 +10,15 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode ptr1=head,ptr2=head;
-        int n=0;
-        while(head!=null){
-            head=head.next;
-            n++;
-            }
-        if(n%2==0){
-            while(ptr2!=null){
-                ptr1=ptr1.next;
-                ptr2=ptr2.next.next;
-            }
-        }else{
-            while(ptr2.next!=null){
-                ptr1=ptr1.next;
-                ptr2=ptr2.next.next;
-            }
-        }
-        return ptr1;
+       ListNode ptr1=head,ptr2=head;
+       // ListNode slow=head;
+
+    while(ptr2!=null && ptr2.next!=null){
+        ptr1=ptr1.next;
+        ptr2=ptr2.next.next;
+
+    }
+    return ptr1;
+
     }
 }
