d26ee40d118318d2d81c1fb100e69dfcb63d9b24
Ankit Singh <ankitsingh.shiva108@gmail.com>
2025-03-02 13:07:06 +0530

    Time: 1 ms (96.52%), Space: 44.1 MB (27.92%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         int size = nums.length;
-        int pointer1 = 0;
  
-        while (pointer1 < size - 1) {
-            if (nums[pointer1] == nums[pointer1 + 1]) {
-                nums[pointer1] *= 2;
-                nums[pointer1 + 1] = 0;
+        for (int i = 0; i < size - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
             }
-            pointer1++;
         }
  
-        int pointer2 = 0; 
-
+        int nonZeroIndex = 0;  
+ 
         for (int i = 0; i < size; i++) {
             if (nums[i] != 0) {
-                nums[pointer2++] = nums[i];
+                nums[nonZeroIndex++] = nums[i];
             }
         }
-
-         
-        for (int i = pointer2; i < size; i++) {
+ 
+        for (int i = nonZeroIndex; i < size; i++) {
             nums[i] = 0;
         }
 
